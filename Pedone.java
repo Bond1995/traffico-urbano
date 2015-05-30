0692e1832ca676ed28ab1d9017c51977d49d25bd
claduio <c.romano007@studenti.unibs.it>
2015-05-30 16:13:13 +0200

    Update Pedone.java

--- Pedone.java
+++ Pedone.java
@@ -1,17 +1,18 @@
 public class Pedone extends Elemento {
 
 	private final static int VELOCITA_PEDONE = 1;
+	private final static int PRIORITY_PEDONE = 2;
 	private final static String PEDONE = "*";
 	
 	public Pedone(int riga, int colonna, int priority, String placeholder) {
 		
-		super(riga,colonna,priority, PEDONE);
+		super(riga,colonna,PRIORITY_PEDONE, PEDONE);
 		
 	}
 	
 	public Pedone(int priority, String placeholder) {
 		
-		super(priority, PEDONE);
+		super(PRIORITY_PEDONE, PEDONE);
 		
 	}
 
@@ -29,4 +30,5 @@ public class Pedone extends Elemento {
 	
 
 }
+
 
