44ee29191948bbd1ef6dcd15ee7f21d63e0a1114
Bond1995 <m.bondaschi@studenti.unibs.it>
2015-05-30 13:53:31 +0200

    Update Elemento.java

--- Elemento.java
+++ Elemento.java
@@ -1,23 +1,90 @@
 public abstract class Elemento {
+
+	private Coordinate posizione;
+	private int priority;
+	private String placeholder;
 	
-	protected int x, y, velocità;
-	
-	public Elemento( int x, int y, int velocità){
+	public Elemento(int riga, int colonna, int priority, String placeholder) {
 		
-		this.x = x;
-		this.y = y;
-		this.velocità = velocità;
+		this.posizione = new Coordinate(riga, colonna);
+		this.priority = priority;
+		this.placeholder = placeholder;
 		
 	}
 	
-	public abstract String toString();
+	public Elemento(int priority, String placeholder) {
+		
+		this.posizione = new Coordinate(0, 0);
+		this.priority = priority;
+		this.placeholder = placeholder;
+		
+	}
 	
-	public abstract int getAscissa();
+	public Elemento() {
+		
+		this.posizione = new Coordinate(0, 0);
+		this.priority = 0;
+		this.placeholder = " ";
+		
+	}
 	
-	public abstract int getOrdinata();
+	public int getRiga() {
+		
+		return posizione.getRiga();
+		
+	}
+	
+	public int getColonna() {
+		
+		return posizione.getColonna();
+		
+	}
+	
+	public void setRiga(int riga) {
+		
+		posizione.setRiga(riga);
+	
+	}
+	
+	public void setColonna(int colonna) {
+		
+		posizione.setColonna(colonna);
+		
+	}
+	
+	@Override
+	public String toString() {
+		
+		return placeholder;
+		
+	};
+	
+	public int getPriority() {
+		
+		return priority;
+		
+	}
+	
+	public String toStringPosizione() {
+		
+		return posizione.toString();
+		
+	}
 	
 	public abstract void move();
 	
+	public boolean prevaleSu(Elemento e) {
+		
+		if (this.priority > e.priority) return true;
+		return false;
+		
+	}
 	
-
+	public boolean collisioneCon(Elemento e) {
+		
+		if (this.posizione.equals(e.posizione)) return true;
+		return false;
+		
+	}
+	
 }
